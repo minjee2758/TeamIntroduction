a4440d39116420b51e2923f1a74818a8a75e0d7f
dawn0920 <saoul0920@gmail.com>
2025-02-19 09:17:20 +0900

    게시글 js 옮기기

---
+++ post.js
@@ -0,0 +1,46 @@
+$(document).ready(function () {
+    // 버튼 클릭 시 모달 열기
+    $("#openModel").click(function () {
+        $("#myModal").css("display", "flex");
+    });
+
+    // 닫기 버튼 클릭 시 모달 닫기
+    $(".close").click(function () {
+        $("#myModal").css("display", "none");
+    });
+
+    $("#postingbtn").click(function () {
+        let isValid = true;
+
+        let title = $("#title").val();
+        let content = $("#content").val();
+        let link = $("#link").val();
+        let file = $("#file")[0].files.length;
+
+        if (!title) {
+            alert("제목을 입력해주세요.");
+            isValid = false;
+        }
+
+        if (!content) {
+            alert("내용을 입력해주세요.");
+            isValid = false;
+        }
+
+        if (!link) {
+            alert("링크를 입력해주세요.");
+            isValid = false;
+        }
+
+        if (file === 0) {
+            alert("파일을 업로드해주세요.");
+            isValid = false;
+        }
+
+        if (isValid) {
+            alert("게시글이 작성되었습니다.");
+            $("#myModal").css("display", "none");
+        }
+
+    });
+});
